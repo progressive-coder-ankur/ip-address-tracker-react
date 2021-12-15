ac616eb7b2950894bc7c3ebc955236bc35dce273
Ankur Singh <abishek.singh.chauhan@gmail.com>
2021-12-15 18:07:36 +0545

    Ui Mostly done , fetch not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,53 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import icon from "./constants";
-import "./App.css";
 import "leaflet/dist/leaflet.css";
+import Details from "./Details";
 
 const defultCenter = [0, 0];
 const defaultZoom = 13;
-
+let fetchedData;
 function App() {
-  function LocationMarker() {
-    const [position, setPosition] = useState(null);
-    const [bbox, setBbox] = useState([]);
+  const [ip, setIp] = useState("");
+  const [apiUrl, setApiUrl] = useState(
+    "https://geo.ipify.org/api/v2/country,city?apiKey=at_CGJTd6vc7ck6dpfEafT0tqrASyb0O"
+  );
+  const [data, setData] = useState(null);
 
-    const map = useMap();
-
-    useEffect(() => {
-      map.locate().on("locationfound", function (e) {
-        setPosition(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
-        const radius = e.accuracy;
-        const circle = L.circle(e.latlng, radius);
-        circle.addTo(map);
-        setBbox(e.bounds.toBBoxString().split(","));
+  const getIP = async (Url) => {
+    await fetch(Url, { method: "GET" })
+      .then((response) => response.json())
+      .then((result) => {
+        return (fetchedData = result);
       });
-    }, [map]);
+    setData({ ...fetchedData });
+  };
 
-    return position === null ? null : (
-      <Marker position={position} icon={icon}>
-        <Popup>
-          You are here. <br />
-          Map bbox: <br />
-          <b>Southwest lng</b>: {bbox[0]} <br />
-          <b>Southwest lat</b>: {bbox[1]} <br />
-          <b>Northeast lng</b>: {bbox[2]} <br />
-          <b>Northeast lat</b>: {bbox[3]}
-        </Popup>
-      </Marker>
-    );
-  }
+  useEffect(() => {
+    Promise.all(getIP(apiUrl));
+  }, []);
 
   return (
-    <div className="App">
-      <div className="hero">
-        <div className="middle">
-          <div className="header">
-            <h1 className="title">IP Address Tracker</h1>
-          </div>
-          <div className="input">
-            <input
-              type="text"
-              placeholder="Search for any IP address or domain"
-            />
-          </div>
-          <div className="details">
-            <div className="ip">
-              <h3 className="heading">IP Address</h3>
-              <p>{"Some Data "}</p>
-            </div>
-            <div className="location">
-              <h3 className="heading">Location</h3>
-              <p>{"Some Data "}</p>
-            </div>
-            <div className="timezone">
-              <h3 className="heading">Timezone</h3>
-              <p>{"Some Data "}</p>
-            </div>
-            <div className="ISP">
-              <h3 className="heading">ISP</h3>
-              <p>{"Some Data "}</p>
-            </div>
-          </div>
+    <div className="App grid">
+      <div className="hero"></div>
+      <div className="middle">
+        <div className="header">
+          <h1 className="title">IP Address Tracker</h1>
         </div>
+        <div className="input flex">
+          <input
+            type="text"
+            className="input"
+            onChange={(e) => setIp(e.target.value)}
+            placeholder="Search for any IP address or domain"
+          />
+          <div
+            className="icon"
+            onClick={() => setApiUrl(apiUrl + "&ipAddress=" + ip)}
+          ></div>
+        </div>
+        <Details data={data} />
       </div>
 
       <div className="map">
@@ -79,24 +55,25 @@ function App() {
           <MapContainer
             center={defultCenter}
             zoom={defaultZoom}
+            maxZoom={16}
             scrollWheelZoom
+            zoomControl={false}
           >
             <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <LocationMarker />
           </MapContainer>
         </div>
       </div>
 
-      <div className="attribution">
+      {/* <div className="attribution">
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
         . Coded by <a href="#">Your Name Here</a>.
-      </div>
+      </div> */}
     </div>
   );
 }
---
+++ src/Details.jsx
@@ -0,0 +1,33 @@
+function Details({ data }) {
+  if (data !== null) {
+    return (
+      <div className="details">
+        <div className="ip flex flex-column">
+          <h3 className="heading">IP Address</h3>
+          <p className="data">{data.ip}</p>
+        </div>
+        <div className="location flex flex-column">
+          <h3 className="heading">Location</h3>
+          <p className="data">
+            {data.location.region}, {data.location.city}
+          </p>
+        </div>
+        <div className="timezone flex flex-column">
+          <h3 className="heading">Timezone</h3>
+          <p className="data">
+            {"UTC "}
+            {data.location.timezone}
+          </p>
+        </div>
+        <div className="ISP flex flex-column">
+          <h3 className="heading">ISP</h3>
+          <p className="data">{data.isp}</p>
+        </div>
+      </div>
+    );
+  } else {
+    return <div className="details">{"Something Went Wrong"}</div>;
+  }
+}
+
+export default Details;
